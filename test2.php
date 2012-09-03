67df30821cc02c12f2fb81b5555958930870d52e
andrey <andrey@e520.localdomain>
2012-09-03 23:16:09 +0300

    Delete test files

--- test2.php
+++
@@ -1,7 +0,0 @@
-<?php
-// test2
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
--- test4.php
+++ test4.php
@@ -1,5 +1,5 @@
 <?php
-// somo file
+// new file test4
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
