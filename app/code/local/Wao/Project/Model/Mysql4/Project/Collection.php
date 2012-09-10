4027341978ad0c6a3d67f1d736064b3239f3d86c
andrey <andrey@e520.localdomain>
2012-09-10 10:34:14 +0300

    Added new theme

--- app/code/local/Wao/Project/Model/Mysql4/Project/Collection.php
+++ app/code/local/Wao/Project/Model/Mysql4/Project/Collection.php
@@ -13,8 +13,8 @@ class Wao_Project_Model_Mysql4_Project_Collection extends Mage_Core_Model_Mysql4
                 ->select()
                 ->from(array('p' => 'wao_projects'));
         
-        $roleId = Mage::helper('project/data')->getUserRoleId();
-        if ($roleId != 1) {
+        $roleName = Mage::getSingleton('admin/session')->getWorkerRole();
+        if ($roleName != 'admin') {
             $select = $select
                 ->join(array('d' => 'wao_developers'), 'p.id = d.id_project', array())
                 ->where('id_user = ?', $userId);
