20d806f8b8de26d5fffe69ced5f387d175dea06a
andrey <andrey@e520.localdomain>
2012-09-17 14:12:51 +0300

    Turned off all Mage modules. Added default roles values.

--- app/code/local/Wao/Project/Block/Adminhtml/Project.php
+++ app/code/local/Wao/Project/Block/Adminhtml/Project.php
@@ -6,8 +6,11 @@ class Wao_Project_Block_Adminhtml_Project extends Mage_Adminhtml_Block_Widget_Co
     public function __construct()
     {
         parent::__construct();
-        $this->_headerText = __('Project');
         
+        $projectId = $this->getRequest()->getParam('pr');
+        $project = Mage::getModel('project/project')->load($projectId);
+        
+        $this->_headerText = $project->getName();
         $this->setTemplate('widget/form/container.phtml');
         
         $this->_addButton('projects', array(
