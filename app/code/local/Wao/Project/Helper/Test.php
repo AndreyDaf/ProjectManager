a1ab17253361bd21caa1ca8ed62f0e26f0736102
andrey <andrey@e520.localdomain>
2014-09-30 20:45:43 +0300

    Added new Test file.

---
+++ app/code/local/Wao/Project/Helper/Test.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *
+ * @category    Mage
+ * @package     Mage_Captcha
+ * @copyright   Copyright (c) 2012 Magento Inc. (http://www.magentocommerce.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Captcha image model
+ *
+ * @category   Mage
+ * @package    Mage_Captcha
+ * @author     Magento Core Team <core@magentocommerce.com>
+ */
+class Wao_Project_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    /**
+     * Get id user which logged in
+     *
+     * @return int
+     */
+    public function getCurrentUserId()
+    {
+        $userSession = Mage::getSingleton('admin/session');
+        
+        return $userSession->getUser()->getId();
+    }
+    
+    /**
+     * Get id role user which logged in
+     *
+     * @return int
+     */
+    public function getUserRoleId()
+    {
+        // Test sdfdsfdsf
+        $userSession = Mage::getSingleton('admin/session');
+        
+        return $userSession->getUser()->getRole()->getRoleId();
+    }
+    
+    /**
+     * Get name role user which logged in
+     *
+     * @return string
+     */
+    public function getUserRoleName()
+    {
+        $userSession = Mage::getSingleton('admin/session');
+        
+        return $userSession->getUser()->getRole()->getRoleName();
+    }
+}
